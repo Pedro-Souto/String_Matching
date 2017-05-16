7511725e4bccca4b90c0fefca7025f0e2df01d34
Pedro Souto <pedro.souto@tecnico.ulisboa.pt>
2017-05-16 21:53:56 +0100

    Quasi Final

--- aa018.c
+++ aa018.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define min(a, b) ((a > b) ? b : a)
-#define max(a, b) ((a < b) ? b : a)
+#define max(x,y) ((x) >= (y)) ? (x) : (y)
+#define min(x,y) ((x) <= (y)) ? (x) : (y)
+#define MAXSIZE 9000
 
 void algoSelector(char* selector, char* target, char* text);
 void naiveAproach(char* target, char* text);
@@ -10,17 +11,18 @@ void KMP(char* target, char* text);
 void KMP_Matcher(char* target, char* text, int size_text_n, int size_target_m);
 int *KMP_Prefix(char* target, int size_target_m);
 void BoyerMoore(char* target, char* text);
-int BoyerMoore_badChar(char* target, int size_target_m, char alphabet, int position);
+int *BoyerMoore_PreProcess_badChar(char* target, int size_target_m);
 int *BoyerMoore_goodSuffix(char* target, int size_target_m);
 void BoyerMoore_Matcher(char* target, char* text, int size_text_n, int size_target_m);
+int checkerBM(char letra);
 
 int main(int argc, char *argv[]) {
 
-	char* target = NULL;
-	char* text = NULL;
+	char* target = (char*)malloc(sizeof(char));
+	char* text = (char*)malloc(sizeof(char));
 	char* auxiliar = NULL;
 	FILE *stream;
-	char line[10240];
+	char line[MAXSIZE];
 
 	stream = stdin;
 	if (stream == NULL) {
@@ -42,6 +44,7 @@ int main(int argc, char *argv[]) {
 			
 	    	if (0 == strncmp(&(line[i]),comutador_text,size)) {
 
+	    		free(text);
 	    		auxiliar = memmove(&line[0], &line[size],(1+ strlen(line)-size));
 	    		auxiliar[strlen(auxiliar)-1] = 0;
 	    		text = (char*)calloc(strlen(auxiliar), sizeof(char));
@@ -54,6 +57,7 @@ int main(int argc, char *argv[]) {
 	    		target = (char*)calloc(strlen(auxiliar), sizeof(char));
 	    		strcpy(target, auxiliar);
 	    		algoSelector(comutador_naive, target, text);
+	    		free(target);
 
 	    	} else if (0 == strncmp(&(line[i]),comutador_KMP,size)) {
 
@@ -62,6 +66,7 @@ int main(int argc, char *argv[]) {
 	    		target = (char*)calloc(strlen(auxiliar), sizeof(char));
 	    		strcpy(target, auxiliar);
 	    		algoSelector(comutador_KMP, target, text);
+	    		free(target);
 
 	    	} else if (0 == strncmp(&(line[i]),comutador_BM,size)) {
 
@@ -70,6 +75,7 @@ int main(int argc, char *argv[]) {
 	    		target = (char*)calloc(strlen(auxiliar), sizeof(char));
 	    		strcpy(target, auxiliar);
 	    		algoSelector(comutador_BM, target, text);
+	    		free(target);
 
 	    	} else {
 	    		algoSelector(comutador_exit, target, text);
@@ -78,9 +84,7 @@ int main(int argc, char *argv[]) {
 	}
 
 	free(text);
-	free(target);
 	fclose(stream);
-	exit(EXIT_SUCCESS);
     return 0;
 }
 
@@ -121,106 +125,140 @@ void KMP(char* target, char* text){
 
 int *KMP_Prefix(char* target, int size_target_m){
 
+	int q = 0,k = 0;
 	int *prefix_pi = malloc(sizeof(int)*size_target_m);
-	int k = 0, q;
-	prefix_pi[0] = k;
 
-	for (q = 2; q <= size_target_m; q++) {
-		while(k > 0 && target[k] != target[q-1])
-			k = prefix_pi[k];
-		if (target[k] == target[q-1])
-			k++;
-		prefix_pi[q-1] = k;
+	prefix_pi[q]=0;
+	for (q = 1; q < size_target_m; q++)	{
+		while ( k > 0 && target[k] != target[q]) k = prefix_pi[k-1];
+		if (target[k] == target[q]) k++;
+		prefix_pi[q] = k;
 	}
 	return prefix_pi;
 }
 
 void KMP_Matcher(char* target, char* text, int size_text_n, int size_target_m){
 
-	int q = 0, i = 0, cout = 0;;
+	int q = 0, i = 0, cout = 0;
 	int *prefix_pi = KMP_Prefix(target, size_target_m);
 
-	while (i < size_text_n)	{
-		if (target[q] == text[i]){
-			if (q == size_target_m - 1){
-				printf("%d ", i - q);
-				i = i - q;
-				q = 0;
-			}
-			else
-				q++;
-			i++;
+	while (i < size_text_n) {
+		cout++;
+		while (q > 0 && target[q] != text[i]) {
+			q = prefix_pi[q-1];
 			cout++;
-		}else{
-			i += prefix_pi[q] + 1 - q;
-			q = 0;
 		}
+		if (target[q] == text[i]) {
+			q++;
+		}
+		if (q == size_target_m) {
+			printf("%d ", i - size_target_m +1);
+			q = prefix_pi[q-1];
+			if (i + 1 < size_text_n) cout++;
+		}
+		i++;
 	}
-	printf("\n%d \n", cout);
-	free(prefix_pi);
+
+    printf("\n%d \n", cout);
+    free(prefix_pi);
 }
 
 void BoyerMoore(char* target, char* text){
 
 	int size_target_m = strlen(target);
 	int size_text_n = strlen(text);
-
+	/*printf("|%i||%i|\n",size_text_n,size_target_m );*/
 	BoyerMoore_Matcher(target, text, size_text_n, size_target_m);
 }
 
-int BoyerMoore_badChar(char* target, int size_target_m, char alphabet, int position){
-	
-	int i;
-	for (i = position; i > -1; i--) {
-		if (0 == strncmp(&(target[i]),&alphabet,1))
-			return  size_target_m - i - 1;
+int *BoyerMoore_PreProcess_badChar(char* target, int size_target_m){
+	int i, j;
+	int n = 4;
+	char alphabet[4] = {'A','C','G','T'};
+	int *BadChar = malloc(sizeof(int)*(size_target_m));
+
+	for (i = 0; i < n; i++){
+		BadChar[i] = size_target_m;
+		for (j = size_target_m-1; j >= 0; j--) {
+			if (0 == strncmp(&alphabet[i],&target[j],1)){
+				BadChar[i] += - j - 1;
+				break;
+			}
+		}
 	}
-	return size_target_m;
+	return BadChar;
 }
 
 int *BoyerMoore_goodSuffix(char* target, int size_target_m){
-	int j=0, i=0, k=0;
-	int *goddSuffix = malloc(sizeof(int)*(size_target_m));
+	int g = 0, i = 0, j = 0, f = 0;
+	int *goodSuffix_auxiliar = malloc(sizeof(int)*(size_target_m));
+	int *move_goodSuffix = malloc(sizeof(int)*(size_target_m));
 
-	for (i = size_target_m - 1; i > -1; i--) {
-		k++;
-		goddSuffix[i] = size_target_m;
-		for(j = i -1; j > -1; j--){
-			if (0 == strncmp(&(target[j]),&(target[i]),k)){
-				if (target[j-1] != target[i-1] && j != 0) {
-					goddSuffix[i] -= j;
-					break;
-				}				
-			}
+	goodSuffix_auxiliar[size_target_m-1] = size_target_m;
+	g = size_target_m - 1;
+
+	for(i = size_target_m - 2; i >= 0; i--){
+		if(i > g && goodSuffix_auxiliar[i + size_target_m -1 - f] < i-g)
+			goodSuffix_auxiliar[i] = goodSuffix_auxiliar[i + size_target_m - 1 - f];
+		else {
+			if(i < g)
+				g = i;
+			f = i;
+			while( g >= 0 && target[g] == target[g + size_target_m - 1 -f]) --g;
+			goodSuffix_auxiliar[i] = f - g;
 		}
 	}
 
-	return goddSuffix;
+	for (i = 0; i <size_target_m; ++i){
+   		move_goodSuffix[i] = size_target_m;
+	}
+   	
+   	for (i =size_target_m - 1; i >= 0; --i){
+   		if (goodSuffix_auxiliar[i] == i + 1){
+        	for (j = 0; j <size_target_m - 1 - i; ++j){
+            	if (move_goodSuffix[j] == size_target_m) 
+            		move_goodSuffix[j] = size_target_m - 1 - i;
+   			}
+        }
+    }
+    for (i = 0; i <=size_target_m - 2; ++i){
+      move_goodSuffix[size_target_m - 1 - goodSuffix_auxiliar[i]] = size_target_m - 1 - i;
+    }
+
+	free(goodSuffix_auxiliar);
+	return move_goodSuffix;
 }
 
 void BoyerMoore_Matcher(char* target, char* text, int size_text_n, int size_target_m){
-
-	int cout = 0;
-	int i = size_target_m - 1;
-	int j = i;
+	int j = 0, i, cout = 0;
+	int *move_badChar = BoyerMoore_PreProcess_badChar(target, size_target_m);
 	int *move_goodSuffix = BoyerMoore_goodSuffix(target, size_target_m);
 
-	while (i < size_text_n){
-		++cout;
-		if (target[j] == text[i]){
-			if (j == 0)	{
-				printf("%d ", i);
-				i += size_target_m;
-				j = size_target_m - 1;
-			} else{
-				i--;
-				j--;
+	while(j <= size_text_n - size_target_m){
+		for (i = size_target_m - 1; i >= 0 && target[i] == text[i+j]; --i) cout++;
+		if (i < 0) {
+			printf("%d ", j);
+			j += move_goodSuffix[0];
+		} else{
+			cout++;
+			if(i+j < size_text_n){
+				j += max(move_goodSuffix[i], move_badChar[checkerBM(text[i+j])] - size_target_m + 1 + i);
 			}
-		}else{
-			i += min(BoyerMoore_badChar(target,size_target_m,text[i],j),move_goodSuffix[j]);
-			j = size_target_m - 1;
 		}
 	}
+
 	printf("\n%d \n",cout);
 	free(move_goodSuffix);
+	free(move_badChar);
+}
+
+int checkerBM(char letra){
+	int i;
+	char alphabet[4] = {'A','C','G','T'};
+	for (i = 0; i < 4; i++) {
+		if(letra == alphabet[i]) {
+			return i;
+		}
+	}
+	return -1;
 }
